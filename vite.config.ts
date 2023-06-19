0cfdf53b49f1bd36f9cd34927ee876dfe57face1
farooq-azam-khan <khanazamfarooq99@gmail.com>
2023-06-19 11:42:47 -0400

    added proxy

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,18 @@ import { defineConfig } from 'vite'
 import elmPlug from 'vite-plugin-elm'
 
 export default defineConfig({
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:8000',
+                changeOrigin: true,
+                rewrite: (path) => {
+                    console.log('running path2', path)
+                    return path// .replace(/^\/api/, '')
+                }
+
+            }
+        }
+    },
     plugins: [elmPlug()]
 })
