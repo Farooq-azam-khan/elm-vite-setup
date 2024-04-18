5809bfb10d6c3296dd8f4cd2a6ebe0ec651df263
farooq-azam-khan <khanazamfarooq99@gmail.com>
2024-04-17 21:18:24 -0400

    route.elm

---
+++ src/Route.elm
@@ -0,0 +1,29 @@
+module Route exposing (..)
+
+import Url exposing (Url)
+import Url.Parser as UP
+
+
+type Route
+    = HomeRoute
+    | UserRoute String
+    | NotFound
+
+
+parse_url : Url -> Route
+parse_url url =
+    case UP.parse match_route url of
+        Just route ->
+            route
+
+        Nothing ->
+            NotFound
+
+
+match_route : UP.Parser (Route -> a) a
+match_route =
+    UP.oneOf
+        [ UP.map HomeRoute UP.top
+        , UP.map User UP.string
+        ]
+
