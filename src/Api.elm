883ecdc334253943e823c6d48131d384485546d5
farooq-azam-khan <khanazamfarooq99@gmail.com>
2024-04-17 20:49:47 -0400

    elm format

--- src/Api.elm
+++ src/Api.elm
@@ -1,23 +1,25 @@
-module Api exposing (..) 
+module Api exposing (..)
 
-
-import Http 
+import Http
 import HttpBuilder
-import Json.Decode as D 
-import Json.Encode as E 
-import RemoteData  exposing (WebData) 
+import Json.Decode as D
+import Json.Encode as E
+import RemoteData exposing (WebData)
+
 
 encode_user : E.Value
-encode_user = E.object [("email", E.string "test@gmail.com"), ("username", E.string "test")]
+encode_user =
+    E.object [ ( "email", E.string "test@gmail.com" ), ( "username", E.string "test" ) ]
 
 
 get_users_api : (WebData (List String) -> msg) -> Cmd msg
-get_users_api msg = 
-    HttpBuilder.post "/api/user" 
-        |> HttpBuilder.withJsonBody encode_user 
+get_users_api msg =
+    HttpBuilder.post "/api/user"
+        |> HttpBuilder.withJsonBody encode_user
         |> HttpBuilder.withTimeout 10000
-        |> HttpBuilder.withExpect 
-            (Http.expectJson 
-                (RemoteData.fromResult >> msg) (D.list D.string)
+        |> HttpBuilder.withExpect
+            (Http.expectJson
+                (RemoteData.fromResult >> msg)
+                (D.list D.string)
             )
         |> HttpBuilder.request
