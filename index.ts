0aa8b5ab7d0bf4822b70adb8fed39e5329433faa
farooq-azam-khan <khanazamfarooq99@gmail.com>
2024-04-17 21:07:05 -0400

    save user settings to local storage

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import './tailwind.css'
 import { Elm } from './src/Main.elm' 
 
+const LOCAL_STORAGE_APP_NAME_PREFIX = 'webux'
+const user_settings_key = `${LOCAL_STORAGE_APP_NAME_PREFIX}_user_settings`
+
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('app')
     if (!root) { 
@@ -9,6 +12,10 @@ document.addEventListener('DOMContentLoaded', () => {
     } 
     const app = Elm.Main.init({
           node: root 
+    })
+
+    app.ports.save_user_settings((message: any) => {
+        localStorage.setItem(user_settings_key, JSON.stringify(message))
     })
     
 })
